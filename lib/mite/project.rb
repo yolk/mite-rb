3376ed4e8922a2e4149352cbc2d96ed4e9db0f29
Sebastian <sebastian@yo.lk>
2009-11-06 00:44:39 +0100

    Added Project#name_with_customer for best practice/convenience

--- lib/mite/project.rb
+++ lib/mite/project.rb
@@ -5,7 +5,11 @@ class Mite::Project < Mite::Base
   def time_entries(options = {})
     Mite::TimeEntry.find(:all, :params => options.update(:project_id => id))
   end
-
+  
+  def name_with_customer
+    customer_name.blank? ? name : "#{name} (#{customer_name})"
+  end
+  
   def customer
     @customer ||= Mite::Customer.find(customer_id) unless customer_id.blank?
   end
